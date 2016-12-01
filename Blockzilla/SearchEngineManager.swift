84fac22ff712ee7df7bbf4d0efd30f8c9307379d
Brian Nicholson <bnicholson@mozilla.com>
2016-11-30 18:05:27 -0800

    Use OpenSearch engines in the project

---
+++ Blockzilla/SearchEngineManager.swift
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+
+class SearchEngineManager {
+    var activeEngine: SearchEngine {
+        return engines.first!
+    }
+
+    lazy var engines: [SearchEngine] = {
+        let enginesPath = Bundle.main.path(forResource: "SearchEngines", ofType: "plist")!
+        let engineMap = NSDictionary(contentsOfFile: enginesPath) as! [String: [String]]
+        let engines = self.searchPaths.flatMap { engineMap[$0] }.first!
+        let parser = OpenSearchParser(pluginMode: true)
+        let pluginsPath = Bundle.main.url(forResource: "SearchPlugins", withExtension: nil)!
+
+        return engines.flatMap { name in
+            let path = self.searchPaths
+                           .map({ pluginsPath.appendingPathComponent($0).appendingPathComponent(name + ".xml") })
+                           .first { FileManager.default.fileExists(atPath: $0.path) }!
+            return parser.parse(file: path)
+        }
+    }()
+
+    private lazy var searchPaths: [String] = {
+        var components = Locale.preferredLanguages.first!.components(separatedBy: "-")
+        if components.count == 3 {
+            components.remove(at: 1)
+        }
+        return [components.joined(separator: "-"), components[0], "default"]
+    }()
+}
